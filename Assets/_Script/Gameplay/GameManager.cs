924f0af28e908b67d7a67124f43c871a3e3c470d
MoDémoxy <82284813+MoDemoxy@users.noreply.github.com>
2024-06-06 20:01:07 -0400

    Move Logic in LogicManager

--- Assets/_Script/Gameplay/GameManager.cs
+++ Assets/_Script/Gameplay/GameManager.cs
@@ -12,52 +12,25 @@ public class GameManager : Singleton<GameManager>
 {
     [SerializeField]
     private VisualManager visualManager;
-
-    //private LogicManager logicManager;
-
-
-    //TO IMPLEMENT IN LOGIC ----------------
-    private readonly Board _board = new();
-    private readonly BitBoard _currentBitBoard = new();
-    private Vector2 _currentPiece;
-    // --------------------------------
+    private readonly LogicManager _logicManager = new();
     
     public UnityEvent<List<Vector2>> onPieceSelected = new ();
 
-    private void Start()
-    {
-        //logicManager = new LogicManager();
-    }
-
     public void SelectPiece(Vector2 position)
     {
-        _currentPiece = position;
-
-        //call to logic to get available position on board => logicManager.GetValidMoves(positionSelectedPiece);
-        List<Vector2> pieces = _currentBitBoard.GetValidMoves();
-
+        //call to logic to get available position on board
+        var pieces = _logicManager.SelectPiece(position);
+        
         //Call event to notify visualManager
         onPieceSelected.Invoke(pieces);
     }
     
     public MoveState MoveTo(int x, int y)
     {
-        var newPosition = new Vector2(x, y);
-        //call to logic to move piece on board => return logicManager.MoveTo(newPosition);
+        
+        //call to logic to move piece on board
+        return _logicManager.MoveTo(x, y);
+        
         //Update visual according to returned enum
-
-        if (_currentBitBoard.Get(newPosition))
-        {
-            return _board.Move(_currentPiece, newPosition) ? MoveState.Eaten : MoveState.Success;
-        }
-
-        return MoveState.Failed;
     }
-
-
-    //TO IMPLEMENT IN LOGIC ----------------
-    public Board GetBoard() { return _board; }
-    public BitBoard GetCurrentBitBoard() { return _currentBitBoard; }
-    public Vector2 GetCurrentPiece() { return _currentPiece; }
-    // --------------------------------
 }
---
+++ Assets/_Script/Gameplay/Logic/LogicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicManager
+{
+    private readonly Board _board = new();
+    private readonly BitBoard _currentBitBoard = new();
+    private Vector2 _currentPiece;
+    
+    public Board GetBoard() { return _board; }
+    public BitBoard GetCurrentBitBoard() { return _currentBitBoard; }
+    public Vector2 GetCurrentPiece() { return _currentPiece; }
+
+    public List<Vector2> SelectPiece(Vector2 position)
+    {
+        _currentPiece = position;
+        //TODO Manage Valid Moves
+        return _currentBitBoard.GetValidMoves();
+    }
+    
+    public MoveState MoveTo(int x, int y)
+    {
+        var newPosition = new Vector2(x, y);
+        
+        if (_currentBitBoard.Get(newPosition))
+        {
+            return _board.Move(_currentPiece, newPosition) ? MoveState.Eaten : MoveState.Success;
+        }
+
+        return MoveState.Failed;
+    }
+
+
+   
+}
